605bb89a1a83cdd4b2c3cdeed6f784fb28437c27
Balakumaran <balakumaran@cloudrevelinnovation.com>
2025-01-17 17:53:59 +0530

    saw

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactss Vishwa
+          Learn React Leo
         </a>
       </header>
     </div>
